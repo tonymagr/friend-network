5bf7fba72dbbe2e680ab3064f0b9d5104e8c9f28
Tony Magrady <tonymagrady@gmail.com>
2024-02-01 22:45:20 -0800

    added clearCollection.js to clear all data for fresh start, and easy command t run it - ready for demo

---
+++ utils/clearCollections.js
@@ -0,0 +1,23 @@
+
+// Clear collections for fresh start on data creation
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Delete the collections if they exist
+  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (userCheck.length) {
+    await connection.dropCollection('users');
+  }
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
+
+  console.info('Data clear is complete.');
+  process.exit(0);
+});
